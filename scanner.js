91ef0c4da6aaac7eaaa3a73b211872e9804d0c5c
Martin <43607388+elconde01@users.noreply.github.com>
2026-01-12 09:42:56 -0300

    Add files via upload

---
+++ scanner.js
@@ -0,0 +1,13 @@
+const qrReader = new Html5Qrcode("reader");
+
+function startScanner(onScan) {
+  qrReader.start(
+    { facingMode: "environment" },
+    { fps: 10, qrbox: 250 },
+    qrText => {
+      qrReader.stop();
+      onScan(qrText);
+    },
+    error => {}
+  );
+}
